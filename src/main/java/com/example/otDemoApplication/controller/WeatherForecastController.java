78f62a8ee5a9182e48aef812c4f263df835c324d
berkaysimsek <berkay.simsek@topia.com>
2020-05-22 18:56:17 +0300

    deleted unused line

--- src/main/java/com/example/otDemoApplication/controller/WeatherForecastController.java
+++ src/main/java/com/example/otDemoApplication/controller/WeatherForecastController.java
@@ -1,16 +1,13 @@
 package com.example.otDemoApplication.controller;
 
 import com.example.otDemoApplication.client.ForecastClient;
-import com.example.otDemoApplication.model.ForecastsDto;
 import com.example.otDemoApplication.model.WeatherDetailDto;
 import com.example.otDemoApplication.service.ForecastService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 public class WeatherForecastController {
